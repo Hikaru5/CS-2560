93a7eb893fde76809a4fdebd23b0438e6d00512a
Hikaru5 <hikarukun5@gmail.com>
2019-01-23 13:15:17 -0800

    Update hwOne.java

--- hwOne.java
+++ hwOne.java
@@ -3,6 +3,6 @@ public class hwOne
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello, my name is Connor Chase");
+		System.out.println("Goodbye, I finished my CS 2560 HW1");
 	}
 }
